d191708a5afae41d9b9957c7b52ec0d7e8d5dae8
gerson arce <arceger@hotmail.com>
2021-03-11 22:35:14 -0300

    new3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="pt-BR">
+<!--html lang="{{ str_replace('_', '-', app()->getLocale()) }}"-->
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>COMUNIDADE HAITIANA</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -129,4 +130,4 @@
             </div>
         </div>
     </body>
-</html>
+</-html>
