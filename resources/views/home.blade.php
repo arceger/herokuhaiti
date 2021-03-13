04d7fe1f1b58dcce9eaad7b988eb7f7a7fd94b5f
gerson arce <arceger@hotmail.com>
2021-03-13 18:40:29 -0300

    new6

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,7 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
+   <h1>LOGADO NO SISTEMA</h1>
                     {{ __('You are logged in!') }}
                 </div>
             </div>
