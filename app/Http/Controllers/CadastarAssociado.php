5b98c71f6f537ea71daeda6b01a2d399ef10dae6
gerson arce <arceger@hotmail.com>
2021-03-20 22:00:43 -0300

    ajust8

---
+++ app/Http/Controllers/CadastarAssociado.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CadastarAssociado extends Controller
+{
+    public function  create(){
+
+        return view ('home');
+    }
+
+    public function store (Request $request) {
+
+        Associado::create([
+
+        'nome' => $request->nome,
+        'sobrenome' => $request->sobrenome,
+        'endereco' => $request->endereco,
+        'telefone' => $request->telefone,
+        'email' => $request->email,
+        'telefone_rec' => $request ->telefone_rec,
+        'cidade' => $request ->cidade,
+        'estado' => $request -> estado,
+        'description' => $request -> description,
+        ]);
+
+        return "Cadastrado com sucesso" ;
+
+        dd($request->all());
+    }
+}
