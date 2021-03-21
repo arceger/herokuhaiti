7730a4f6d6b72898471d562ef8f5784470f99455
gerson arce <arceger@hotmail.com>
2021-03-21 20:28:35 -0300

    ajust233

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -36,4 +36,36 @@ class HomeController extends Controller
         return view('cadastrar');
     }
 
+    public function  create(){
+
+        return view ('cadastrar');
+
+       
+    }
+
+    public function store (Request $request) {
+
+
+        dd($request->all());
+
+/*
+        Associado::create([
+
+        'nome' => $request->nome,
+        'sobrenome' => $request->sobrenome,
+        'endereco' => $request->endereco,
+        'telefone' => $request->telefone,
+        'email' => $request->email,
+        'telefone_rec' => $request ->telefone_rec,
+        'cidade' => $request ->cidade,
+        'estado' => $request -> estado,
+        'description' => $request -> description,
+        ]);
+*/
+        return "Cadastrado com sucesso" ;
+
+       
+    }
+
+
 }
