93c99eb3218a4772ee8466eac7e919f1b9b39517
gerson arce <arceger@hotmail.com>
2021-03-21 14:29:01 -0300

    ajust25

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,7 +17,7 @@
                       <div class="card-header" align="center">{{ __('Continue seu Cadastro') }}</div>
 
                        <div-- class="card-body">
-                       <form method="POST" action="{{ route('CadastarAssociado') }}">
+                       <form method="POST" action="{{ route('register') }}">
                         @csrf
 
 
@@ -121,7 +121,7 @@
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="description" class="col-md-4 col-form-label text-md-right">{{ __('Sobre Voce') }}</label>
+                            <label for="description" class="col-md-4 col-form-label text-md-right">{{ __('Sobre') }}</label>
 
                             <div class="col-md-6">
                                 <input id="description" type="text" class="form-control" name="description" required autocomplete="description">
