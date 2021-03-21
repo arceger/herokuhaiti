f1e0c6e774d3d683db415b5860ff60ec5f577e69
gerson arce <arceger@hotmail.com>
2021-03-21 14:24:52 -0300

    ajust24

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,7 +17,7 @@
                       <div class="card-header" align="center">{{ __('Continue seu Cadastro') }}</div>
 
                        <div-- class="card-body">
-                       <form method="POST" action="{{ route('Associado') }}">
+                       <form method="POST" action="{{ route('CadastarAssociado') }}">
                         @csrf
 
 
