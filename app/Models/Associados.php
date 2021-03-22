21b308385c8ec77dcf08bad190eb34e46cf2359a
gerson arce <arceger@hotmail.com>
2021-03-21 23:27:09 -0300

    ajust233

--- app/Models/Associados.php
+++ app/Models/Associados.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Associado extends Model
+class Associados extends Model
 {
     protected $fillable = ['id','nome','sobrenome','endereco','telefone','telefone_rec','cidade','estado','description','papel'];
    // use HasFactory;
