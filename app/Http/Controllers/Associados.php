1662447a0933b397cd2731fa8eb773fa220d1c99
gerson arce <arceger@hotmail.com>
2021-03-21 23:07:43 -0300

    ajust233

---
+++ app/Http/Controllers/Associados.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Associado extends Model
+{
+    protected $fillable = ['id','nome','sobrenome','endereco','telefone','telefone_rec','cidade','estado','description','papel'];
+   // use HasFactory;
+}
