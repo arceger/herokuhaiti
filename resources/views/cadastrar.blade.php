9b7b71e8d2df37685f00f4eb91939bb146d1a34a
gerson arce <arceger@hotmail.com>
2021-03-21 21:29:58 -0300

    ajust233

--- resources/views/cadastrar.blade.php
+++ resources/views/cadastrar.blade.php
@@ -133,7 +133,8 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Cadastrar') }}
+                                <!--
+                                   {{ __('Cadastrar') }} -->
                                 </button>
                             </div>
                         </div>
