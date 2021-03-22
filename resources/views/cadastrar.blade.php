2ad4a18808029fa3dacbfcb86631e9f7a89d6961
gerson arce <arceger@hotmail.com>
2021-03-21 21:49:52 -0300

    ajust233

--- resources/views/cadastrar.blade.php
+++ resources/views/cadastrar.blade.php
@@ -25,10 +25,10 @@
 
 
                         <div class="form-group row">
-                            <label for="name" value =" fulano" class="col-md-4 col-form-label text-md-right">{{ __('Nome') }}</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nome') }}</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <input id="name" type="text" value ="fulano" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
@@ -77,10 +77,10 @@
 
 
                         <div class="form-group row">
-                            <label for="email" value = "fulano@hotmail.com"class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <input id="email" type="email" value = "fulano@hotmail.com" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
