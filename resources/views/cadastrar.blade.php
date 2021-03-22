b8aee0696830d224c038271ba91f36800cf5d745
gerson arce <arceger@hotmail.com>
2021-03-21 22:43:05 -0300

    ajust233

--- resources/views/cadastrar.blade.php
+++ resources/views/cadastrar.blade.php
@@ -1,42 +1,23 @@
-<DOCTYPE html>
-<html lang = "pt-BR">
+@extends('layouts.app')
 
-<head> 
-<meta charset = "utf-8">
-<title>Form test</title>
-</head>
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
 
-<body>
-<form action= "" >  <!--//{{ route ( 'cadastrar_usr') }}" method = "POST"-->
-
-<laber for = ""> Nome </label> <br/>
-<input type= "text" name="nome" value = "testst"> <br/>
-
-<laber for = ""> Sobre Nome </label> <br/>
-<input type= "text" name="sobrenome"> <br/>
-
-<laber for = ""> Email </label> <br/>
-<input type= "text" name="email"> <br/>
-
-<laber for = ""> Telefone </label> <br/>
-<input type= "text" name="telefone"> <br/>
-
-<laber for = ""> Telenone Rec </label> <br/>
-<input type= "text" name="telefone_rec"> <br/>
-
-<laber for = ""> Cidade </label> <br/>
-<input type= "text" name="cidade"> <br/>
-
-<laber for = ""> Estado </label> <br/>
-<input type= "text" name="estado"> <br/>
-
-<laber for = ""> Sobre Voce</label> <br/>
-<input type= "text" name="description"> <br/>
-
-
-<button> Enviar</button>
-</form>
-
-</body>
-
-</html>
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+   <h1>LOGADO NO SISTEMA</h1>
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
