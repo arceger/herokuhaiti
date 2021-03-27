f58ee314e51f86aa70cc2e286e13517d8d66e7cf
gerson arce <arceger@hotmail.com>
2021-03-27 18:28:02 -0300

    ajust233

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -22,7 +22,10 @@ class HomeController extends Controller
      * @return \Illuminate\Contracts\Support\Renderable
      */
     public function index()
+
     {
+        $name='name';
+        
         return view('home',$name);
     }
 /*$
