1807678081f446968ecd22adb0a5440fa6742caa
gerson arce <arceger@hotmail.com>
2021-03-26 23:16:21 -0300

    ajust233

--- database/migrations/2021_03_20_122650_create_size_vests_table.php
+++ database/migrations/2021_03_20_122650_create_size_vests_table.php
@@ -14,7 +14,7 @@ class CreateSizeVestsTable extends Migration
     public function up()
     {
         Schema::create('size_vests', function (Blueprint $table) {
-            $table->string('id');
+            $table->increments('id');
             $table->string('altura'); //aproximada
             $table->string('cintura');
             $table->string('busto');//manequin
