f2704278fe8fba06998cd55704a8fb91c62adc82
gerson arce <arceger@hotmail.com>
2021-03-27 00:14:16 -0300

    ajust233

--- routes/web.php
+++ routes/web.php
@@ -23,8 +23,8 @@ Auth::routes();
 
 //Route::get('/cadastrar', [App\Http\Controllers\HomeController::class, 'index3'])->name('cadastrar');
 
-Route::get('/caddep', [App\Http\Controllers\CadastrarDepend::class, 'create1']); 
-Route::post('/caddep', [App\Http\Controllers\CadastrarDepend::class, 'store1'])->name('cadastrar_dep');
+Route::get('/caddep', [App\Http\Controllers\CadastrarDepend::class, 'create']); 
+Route::post('/caddep', [App\Http\Controllers\CadastrarDepend::class, 'store'])->name('cadastrar_dep');
 Route::get('/cadastrar', [App\Http\Controllers\CadastrarAssociado::class, 'create']); 
 Route::post('/cadastrar', [App\Http\Controllers\CadastrarAssociado::class, 'store'])->name('cadastrar_usr');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
