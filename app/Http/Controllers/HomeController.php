627c4a78bae386f63ecd67d7446d7ff69e2429f3
gerson arce <arceger@hotmail.com>
2021-03-27 18:12:24 -0300

    ajust233

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,9 +23,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('home',$name);
     }
-/*
+/*$
     public function index3()
     {
         return view('cadastrar');
