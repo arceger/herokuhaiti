f98a4ea7c95df881b3334bf76d14b095342c34ee
gerson arce <arceger@hotmail.com>
2021-03-28 20:44:11 -0300

    html5Imag

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,21 +31,7 @@
 
   </head>
   <body>
-      
-  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
-            @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                    @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
-                    @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
 	<header id="header">
         <nav class="navbar navbar-fixed-top" role="banner">
             <div class="container">
