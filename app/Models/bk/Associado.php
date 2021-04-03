b4bd88b307d541e1b57197587cf69cd986319491
gerson arce <arceger@hotmail.com>
2021-04-03 18:26:10 -0300

    tableGrupo

--- app/Models/bk/Associado.php
+++ app/Models/bk/Associado.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Associado extends Model
 {
     protected $fillable = ['id','nome','est_civel','endereco','email','telefone','telefone_rec','cidade','estado','description'];
-    use HasFactory;
+   use HasFactory;
 }
