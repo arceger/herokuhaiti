878a99b2cca0138d30657053627de908c67edbc6
gerson arce <arceger@hotmail.com>
2021-04-03 17:45:11 -0300

    tableGrupo

--- app/Models/Grupos.php
+++ app/Models/Grupos.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Grupos extends Model
 {
-    protected $fillable = ['id_usr','vacinas_st','vacinas_brief','tipo_sang','alergias','email'];
-    
+    protected $fillable = ['id_usr','categoria','email','nome'];
+
     use HasFactory;
 }
