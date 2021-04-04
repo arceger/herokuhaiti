7997bcfcb80f6029a5f0a33f3f281aa229891e10
gerson arce <arceger@hotmail.com>
2021-04-04 19:12:34 -0300

    read1

--- resources/views/admhome.blade.php
+++ resources/views/admhome.blade.php
@@ -23,7 +23,7 @@
 
                     <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                            <button type="button" style="background-color: #b4b7b7">
+                            <button type="button" style="background-color: #b4b7b7;color: red">
                                <a href="{{ url('https://hidden-harbor-63058.herokuapp.com/') }}">Finalizar Cadastro</a>
                                 </button>
                             </div>
