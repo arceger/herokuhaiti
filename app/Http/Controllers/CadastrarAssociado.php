68dc692e4e5fef2b7ac86450973f7189f7088936
gerson arce <arceger@hotmail.com>
2021-04-05 11:16:43 -0300

    read1

--- app/Http/Controllers/CadastrarAssociado.php
+++ app/Http/Controllers/CadastrarAssociado.php
@@ -43,8 +43,8 @@ class CadastrarAssociado extends Controller
 
     public function show(){
 
-    return Associado::all();
-       // return view('show',['associado'=>$data]) ;
+    $data = Associado::all();
+     return view('show',['associados'=>$data]) ;
     }
 
 
