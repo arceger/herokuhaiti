8db858f0824a39c48fae0c52052d44d7fc059f62
gerson arce <arceger@hotmail.com>
2021-04-05 00:41:02 -0300

    read1

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -7,26 +7,26 @@
     <!--div-- class="row justify-content-center">
         <div class="col-md-8">
                 <div class="form-group row">
-                            <label for="id" maxlength="25" class="col-md-4 col-form-label text-md-right">{{ __('id') }}</label>
+                            <label for="id" maxlength="25" class="col-md-4 col-form-label text-md-right"></label>
 
                             <div class="col-md-6">
-                                <input id="id" type="text" class="form-control" name="id" value = " 1 " required autocomplete="id">
+                                <input id="id" type="text" class="form-control" name="id" value = "" required autocomplete="id">
                             </div>
                         </div>
 
                         <div class="form-group row">
-                            <label for="nome" maxlength="25" class="col-md-4 col-form-label text-md-right">{{ __('nome') }}</label>
+                            <label for="nome" maxlength="25" class="col-md-4 col-form-label text-md-right"></label>
 
                             <div class="col-md-6">
-                                <input id="nome" type="text" class="form-control" value = "{{  }} "name="nome" required autocomplete="nome">
+                                <input id="nome" type="text" class="form-control" value = "" name="nome" required autocomplete="nome">
                             </div>
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" maxlength="25" class="col-md-4 col-form-label text-md-right">{{ __('E mail') }}</label>
+                            <label for="email" maxlength="25" class="col-md-4 col-form-label text-md-right"></label>
 
                             <div class="col-md-6">
-                                <input id="email" type="text" class="form-control" name="email" value= "{{ }}" required autocomplete="email">
+                                <input id="email" type="text" class="form-control" name="email" value= "" required autocomplete="email">
                             </div>
                         </div>
         </div>
