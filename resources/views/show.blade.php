e6a1f6d99d2a97feea13a46ecd9a681423926277
gerson arce <arceger@hotmail.com>
2021-04-05 01:16:46 -0300

    read1

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -2,14 +2,18 @@
 
 @section('content')
 <div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
 
-@if (session('status'))
-<div class="alert alert-success" role="alert">
- {{ session('status') }}
- </div>
- @endif
-
-<h3> lista Associados</he>
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    <h3> lista Associados</he>
     <div class="row justify-content-center">
         <div class="col-md-8">
 
@@ -29,34 +33,8 @@
 </tr>
 
 </table>
-
-
-
-
-
-                <!--div class="form-group row">
-                            <label for="id" maxlength="25" class="col-md-4 col-form-label text-md-right"></label>
-
-                            <div class="col-md-6">
-                                <input id="id" type="text" class="form-control" name="id" value = "" required autocomplete="id">
-                            </div>
-                        </!--div>
-
-                        <div class="form-group row">
-                            <label for="nome" maxlength="25" class="col-md-4 col-form-label text-md-right"></label>
-
-                            <div class="col-md-6">
-                                <input id="nome" type="text" class="form-control" value = "" name="nome" required autocomplete="nome">
-                            </div>
-                        </div>
-
-                        <div-- class="form-group row">
-                            <label for="email" maxlength="25" class="col-md-4 col-form-label text-md-right"></label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="text" class="form-control" name="email" value= "" required autocomplete="email">
-                            </div>
-                        </div-->
+                </div>
+            </div>
         </div>
     </div>
 </div>
