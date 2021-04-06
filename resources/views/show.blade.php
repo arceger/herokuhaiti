7ad83a18c3e2cce468c175e338aafa00559b5f53
gerson arce <arceger@hotmail.com>
2021-04-05 22:44:27 -0300

    read

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -13,26 +13,32 @@
                             {{ session('status') }}
                         </div>
                     @endif
-                    <h3> lista Associados</he>
+                    <h3 align = "center"> Lista de Associados</he>
     <div class="row justify-content-center">
         <div class="col-md-8">
 
-<table border ="1">
+        <table border ="1">
 
-<tr>
-<td>Nome</td>
-<td>E-mail</td>
-<td>Telefone</td>
-</tr>
-@foreach($associados as $associado)
-
-<tr>
-<td>{{$associado['nome']}}</td>
-<td>{{$associado['email']}}</td>
-<td>{{$associado['telefone']}}</td>
-</tr>
-@endforeach
-</table>
+                <tr>
+                     <td>ID</td>
+                     <td>Nome</td>
+                     <td>E-mail</td>
+                     <td>Telefone</td>
+                     <td>Estado Civel</td>
+                     <td>Endereço</td>
+                </tr>
+                @foreach($associados as $associado)
+                
+                <tr>
+                       <td>{{$associado['id']}}</td>
+                       <td>{{$associado['nome']}}</td>
+                       <td>{{$associado['email']}}</td>
+                       <td>{{$associado['telefone']}}</td>
+                       <td>{{$associado['est_civel']}}</td>
+                       <td>{{$associado['endereco']}}</td>
+                </tr>
+                @endforeach
+        </table>
                 </div>
             </div>
         </div>
