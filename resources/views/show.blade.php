1cb2b101433307b286a3daa0dec7353f0cc3fb64
gerson arce <arceger@hotmail.com>
2021-04-05 23:01:53 -0300

    read

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -2,20 +2,20 @@
 
 @section('content')
 <div class="container">
-    <!--div class="row justify-content-center"-->
-        <div class="col-md-8">
-            <div class="card">
-                <!--div class="card-header">{{ __('Dashboard') }}</!--div-->
+             <!--div class="row justify-content-center"-->
+    <!--div class="col-md-8"-->
+        <!--div class="card"-->
+                    <!--div class="card-header">{{ __('Dashboard') }}</!--div-->
 
-                  <div class="card-body">
+                     <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
                     <h3 align = "center"> Lista de Associados</he>
-                  <div class="row justify-content-center">
-                  <div-- class="col-md-8">
+                     <div class="row justify-content-center">
+                     <div-- class="col-md-8">
 
                   <table border ="1">
 
@@ -39,9 +39,9 @@
                     </tr>
                      @endforeach
                    </table>
-                <!--/div-->
-            </div>
-        </div>
-    <!--/div-->
+                   <!--/div-->
 </div>
+    <!--/div-->
+    <!--/div-->
+<!--/div-->
 @endsection
