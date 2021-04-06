0b2872f12b8cfae000169d54471e0be3954727fe
gerson arce <arceger@hotmail.com>
2021-04-05 22:51:04 -0300

    read

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -5,41 +5,41 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <!--div class="card-header">{{ __('Dashboard') }}</!--div-->
 
-                <div class="card-body">
+                  <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
                     <h3 align = "center"> Lista de Associados</he>
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+                  <div class="row justify-content-center">
+                  <div-- class="col-md-8">
 
-        <table border ="1">
+                  <table border ="1">
 
-                <tr>
+                  <tr>
                      <td>ID</td>
                      <td>Nome</td>
                      <td>E-mail</td>
                      <td>Telefone</td>
                      <td>Estado Civel</td>
                      <td>Endereço</td>
-                </tr>
-                @foreach($associados as $associado)
+                  </tr>
+                   @foreach($associados as $associado)
                 
-                <tr>
+                   <tr>
                        <td>{{$associado['id']}}</td>
                        <td>{{$associado['nome']}}</td>
                        <td>{{$associado['email']}}</td>
                        <td>{{$associado['telefone']}}</td>
                        <td>{{$associado['est_civel']}}</td>
                        <td>{{$associado['endereco']}}</td>
-                </tr>
-                @endforeach
-        </table>
-                </div>
+                    </tr>
+                     @endforeach
+                   </table>
+                <!--/div-->
             </div>
         </div>
     </div>
